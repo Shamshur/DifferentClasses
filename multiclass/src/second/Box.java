4cd2529b0514b636d7e4cd4287c020721e321b59
Shamshur <shamshurtheone@gmail.com>
2021-09-20 19:48:48 +0200

    Fourth commit

--- multiclass/src/second/Box.java
+++ multiclass/src/second/Box.java
@@ -3,6 +3,7 @@ package second;
 public class Box {
 	float width;
 	float height;
+//	compute & return value
 	float volume() {
 		return width*height;
 	}
--- multiclass/src/second/Volume.java
+++ multiclass/src/second/Volume.java
@@ -6,10 +6,16 @@ public class Volume {
 		Box firstBox=new Box();
 		Box secondBox=new Box();
 		double vol;
+		double vol1;
 		firstBox.height=10;
 		firstBox.width=33;
+		
+		secondBox.height=21;
+		secondBox.width=22;
 		vol= firstBox.volume();
+		vol1=secondBox.volume();
 		System.out.println(vol);
+		System.out.println(vol1);
 		
 		
 
