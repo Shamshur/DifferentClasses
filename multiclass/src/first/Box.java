a31da2e9f9344dc74981d5436d33201060b443db
Shamshur <shamshurtheone@gmail.com>
2021-09-20 02:23:00 +0200

    Second commit

--- multiclass/src/first/Box.java
+++ multiclass/src/first/Box.java
@@ -2,7 +2,7 @@ package first;
 
 public class Box {
 	double width;
-	double height;{
-	width=22;}
-
+	double height;
+	
 }
+
--- multiclass/src/first/Volume.java
+++ multiclass/src/first/Volume.java
@@ -5,7 +5,7 @@ public class Volume {
 	public static void main(String[] args) {
 		Box myBox=new Box();
 		Box myBox1=new Box();
-		double vol;
+		double vol;  //If i choose int instead of double error populate
 		myBox.width=10;
 		myBox1.height=20;
 		vol=myBox.width*myBox1.height;
